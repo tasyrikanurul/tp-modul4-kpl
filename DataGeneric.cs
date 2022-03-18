efb2e32fb3952c54c417705a73634b76f24c190c
tasyrikanurul <tasyrikanurul@student.telkomuniversity.ac.id>
2022-03-18 14:48:56 +0700

    menambahkan class DataGeneric

---
+++ DataGeneric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302201131
+{
+    class DataGeneric { }
+    class Data<T> : DataGeneric { }
+
+    private static void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: Y");
+    }
+    }
+    
