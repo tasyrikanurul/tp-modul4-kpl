f70e24e0986096ef35150d996304231fec696c56
tasyrikanurul <tasyrikanurul@student.telkomuniversity.ac.id>
2022-03-18 14:00:47 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ HaloGeneric.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302201131
+{
+    public class HaloGeneric <T>
+    {
+       static void SapaUser <T>(ref T lhs, ref T rhs)
+       {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+
+        private static void SapaUser(ref string x)
+        {
+            throw new NotImplementedException();
+            Console.WriteLine("Halo user X");
+        }
+
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Tasy");
+        }
+
+        }
+    }
+
